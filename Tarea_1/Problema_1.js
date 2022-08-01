94c41abed11d4b533c8ba35698bec385d17fcb7a
David-Ostos <davidostos2@gmail.com>
2022-08-01 18:23:55 -0400

    agregando el resultado del alcohol

--- Tarea_1/Problema_1.js
+++ Tarea_1/Problema_1.js
@@ -2,5 +2,9 @@ var litros = 40;
 var kl = 480;
 var resultado = kl / litros;
 
-document.write("Si el tanque es de " + litros + " litros y se hace un recorrido de " + kl + 
+document.write("1- Si el tanque es de " + litros + " litros y se hace un recorrido de " + kl + 
 " kl. <br> El consumo de gasolina es de " + resultado + " kilometros por litro.")
+
+kl = 300
+
+document.write("<br><br>2- El consumo por el recorrido es de " + resultado + "litros <br> de alcohol por los" + kl + "recorridos")
