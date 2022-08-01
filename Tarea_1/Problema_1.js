6b4726a052378deafa267d4257b4add3f19ee7d5
David-Ostos <davidostos2@gmail.com>
2022-08-01 18:02:58 -0400

    codigo final de js

---
+++ Tarea_1/Problema_1.js
@@ -0,0 +1,6 @@
+var litros = 40;
+var kl = 480;
+var resultado = kl / litros;
+
+document.write("si se tiene " + litros + " litros y se hace un recorrido de " + kl + 
+" kl. <br> Se recorre " + resultado + " kilometros por litro")
