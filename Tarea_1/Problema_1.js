9493b112984db439b29fea075d3ef2f9883540c4
David-Ostos <davidostos2@gmail.com>
2022-08-01 18:12:33 -0400

    añadiendo los ultimos detalles

--- Tarea_1/Problema_1.js
+++ Tarea_1/Problema_1.js
@@ -2,5 +2,5 @@ var litros = 40;
 var kl = 480;
 var resultado = kl / litros;
 
-document.write("si se tiene " + litros + " litros y se hace un recorrido de " + kl + 
-" kl. <br> Se recorre " + resultado + " kilometros por litro")
+document.write("Si el tanque es de " + litros + " litros y se hace un recorrido de " + kl + 
+" kl. <br> El consumo de gasolina es de " + resultado + " kilometros por litro.")
