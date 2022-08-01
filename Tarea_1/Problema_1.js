3795c381aabf6c587ca9b485cb6fe6cbbf9c8951
David-Ostos <davidostos2@gmail.com>
2022-08-01 18:30:08 -0400

    agregando la palabra resultados en h3

--- Tarea_1/Problema_1.js
+++ Tarea_1/Problema_1.js
@@ -2,6 +2,8 @@ var litros = 40;
 var kl = 480;
 var resultado = kl / litros;
 
+document.write("<h3>Respuestas</h3>")
+
 document.write("1- Si el tanque es de " + litros + " litros y se hace un recorrido de " + kl + 
 " kl. <br> El consumo de gasolina es de " + resultado + " kilometros por litro.")
 
