f80ca95ee8bc75b8bdb0e27a8f1420cca3137a3a
David-Ostos <davidostos2@gmail.com>
2022-08-04 12:34:34 -0400

    Creando archivos de la tarea 4

---
+++ Tarea_4/tarea.js
@@ -0,0 +1,12 @@
+function salta() {
+
+    document.write("<br><br><hr><br>")
+}
+
+//funcion para escribir y saltar las lineas mas simplificado.
+function imprimir(frase) {
+
+    document.write("<big>" + frase + "</big>");
+        salta();
+
+    }
